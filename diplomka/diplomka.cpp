cc76a5a79609193944c6d588092ba98ae6304879
cernym16 <cernym16@fit.cvut.cz>
2013-01-07 16:45:07 +0100

    přidáno: použití třídy GMP - mpirxx.h hilbertův odhad diskriminantu rozklad M na soucin prvočísel

--- diplomka/diplomka.cpp
+++ diplomka/diplomka.cpp
@@ -10,16 +10,46 @@
 
 using namespace std;
 
+
 int main(int argc, char* argv[])
 {
+	/*mpz_class aBigPO2;
 
-	matice<double> mat(4);
-	mat.load_matrix("mat1.txt");
+    aBigPO2 = 1073741824; //2^30
+    aBigPO2*=aBigPO2; //2^60
+    aBigPO2*=aBigPO2; //2^120
+    aBigPO2*=aBigPO2; //2^240
+    aBigPO2*=aBigPO2; //2^480
+    aBigPO2*=aBigPO2; //2^960
+    aBigPO2*=aBigPO2; //2^1920
+
+    cout << aBigPO2 << endl;
+	//*/
+
+	bool yes=false;
+	unsigned short s=65530;
+	if(s+30>s)
+		yes=true;
+
+	float x=1.0/(float)(1 << 20);
+	
+	int e;
+	float a=frexp(x, &e);
+	int v=(int)(a*(1 << numeric_limits<float>::digits));
+	e-=numeric_limits<float>::digits;
+	yes=false;
+	if(x==ldexp((float)v, e))
+		yes=true;
+
+
+	
+	matice<float> mat(4);
+	mat.load_matrix("mat2.txt");
 	mat.vypsat();
 	mat.do_modular();
 	cout << "VYSLEDEK:" << endl;
 	mat.vypsat();
-	
+	//*/
 	cin.get();
 	return 0;
 }
--- diplomka/matice.h
+++ diplomka/matice.h
@@ -6,7 +6,11 @@
 #include <iostream>
 #include <fstream>
 
-using namespace std; 
+using namespace std;
+
+#pragma warning(disable: 4800; disable: 4244)
+#include <mpirxx.h>
+#pragma warning(default: 4244; default: 4800)
 
 template<class T>
 class matice
@@ -134,11 +138,11 @@ public:
 		{
 			N=newN;
 			if(pointer!=NULL) delete pointer;
-			pointer=new double[N*N];
+			pointer=new T[N*N];
 			if(prava_strana!=NULL) delete prava_strana;
-			prava_strana=new double[N];
+			prava_strana=new T[N];
 		}
-		double a;
+		T a;
 		for(int y=0;y<N;y++)
 		{
 			for(int x=0;x<N;x++)
@@ -203,61 +207,144 @@ public:
 			set_cell(ipivot, ipivot, 1.0);
 		}
 	}
+	int hadamard()
+	{
+		
+	}
 	void do_modular(void)
 	{
-		// frexp, ldexp
 		if(pointer==NULL || prava_strana==NULL) return;
-		// TODO: vygenerovat pole prvocisel
-
-		double D_hadamard=1.0;
-		for(int iX=0;iX<N;iX++)
+		// prenasobeni radku matice, aby za desetinou carkou byly jen nuly
+		for(int y=0;y<N;y++)
 		{
-			double souc=0.0;
-			for(int iY=0;iY<N;iY++)
-			{
-				souc+=get_cell(iX, iY)*get_cell(iX, iY);
-			}
-			// kontrola jestli by slo ze 'souc' vytknout nejake x^2, ktere vyleze pred odmocninu, a ktere deli 'D_hadamard' => nesmi byt m_i
-			D_hadamard*=souc;
-		}
-		D_hadamard=sqrt(D_hadamard);
-
-
-		int min_exponent;		// exponent nejmensiho cisla
-		T max_a=get_cell1(0);
-		T max_y=prava_strana[0];
-		frexp(max_a, &min_exponent);
-		for(int i=1;i<N*N;i++)
-		{
+			int min_exponent;
+			ifrexp(prava_strana[y], &min_exponent);
 			int exponent;
-			frexp(get_cell1(i), &exponent);
-			if(min_exponent>exponent)
+			for(int x=0;x<N;x++)
 			{
-				min_exponent=exponent;
-			}
-			if(max_a<get_cell1(i))
-			{
-				max_a=get_cell1(i);
-			}
-			if(i<N)
-			{
-				if(max_y<prava_strana[i])
+				ifrexp(get_cell(x,y), &exponent);
+				if(min_exponent>exponent)
 				{
-					max_y=prava_strana[i];
+					min_exponent=exponent;
 				}
 			}
+			T pom=frexp(prava_strana[y], &exponent);
+			prava_strana[y]=ldexp(pom, exponent - min_exponent);
+			for(int x=0;x<N;x++)
+			{
+				pom=frexp(get_cell(x,y), &exponent);
+				set_cell(x,y, ldexp(pom, exponent - min_exponent));
+			}
 		}
-		T M1=pow((T)N, N/2.0)*pow(max_a, N);
+
+		// spocitat 'M'
+		T max_a=get_cell(0,0);
+		T max_y=prava_strana[0];
+		for(int y=0;y<N;y++)
+		{
+			for(int x=0;x<N;x++)
+			{
+				if(max_a<get_cell(x,y))
+				{
+					max_a=get_cell(x,y);
+				}
+			}
+			if(max_y<prava_strana[y])
+			{
+				max_y=prava_strana[y];
+			}
+		}
+		T M1=pow((T)N, N/2)*pow(max_a, N);
 		T M2=N*pow((T)(N-1), (N-1)/2)*pow(max_a, N-1)*max_y;
+		T M=2*max(M1, M2);
 
-		T M=2.0*max(M1, M2);
-		// TODO: funkce gcd(M,D), zvolit m_1, m_2... m_r
-		//       nasobit prvocisla, ktera nedeli D_hadamard, do te doby nez budou vetsi nez M
+		int poc_sieve;
+		frexp(M, &poc_sieve);
+		poc_sieve=poc_sieve*poc_sieve;
+		// vygenerovat pole prvocisel
+		int* sieve = new int[poc_sieve];
+		for(int i = 0; i <= poc_sieve; i ++)
+		{
+			sieve[i] = true;
+		}
+		//true == je prvocislo
+		//false == je slozene
+		sieve[0] = sieve[1] = false; //nula a jedna nejsou prvocisla
+		for(int i = 2; i <= sqrt((double)poc_sieve); i++)
+		{
+			//if( i%2==0 ) sieve[i]=false;	// suda cisla nejsou prvocisla a 2 je nevyhovujici
+			if(sieve[i] == false) continue;
+			for(int j = 2*i; j <= poc_sieve; j+=i)
+			{ //samotne citani
+				sieve[j] = false;	// nemuze byt z definice prvocislem (je nasobkem jineho cisla)
+			}
+		}
+		
+		// hadamard
+		mpz_class D_hadamard1=1;
+		for(int iX=0;iX<N;iX++)
+		{
+			mpz_class souc=0;
+			for(int iY=0;iY<N;iY++)
+			{
+				souc+=((long)get_cell(iX, iY))*((long)get_cell(iX, iY));
+			}
+			double dh=D_hadamard1.get_d();
+			double ds=souc.get_d();
+			// kontrola jestli by slo ze 'souc' vytknout nejake x^2, ktere vyleze pred odmocninu, a ktere deli 'D_hadamard' => nesmi byt m_i
+			mpz_class delitel;
+			mpz_gcd(delitel.get_mpz_t(), souc.get_mpz_t(), D_hadamard1.get_mpz_t());
+			int idelitel=(int)delitel.get_d();
+			//sieve[idelitel]=false;	// nevyhovujici prvocislo/slozene cislo
+			//D_hadamard1/=delitel;
+			//souc/=delitel;
+			D_hadamard1*=souc;
+		}
+		
+		mpz_class D_hadamard;
+		mpz_sqrt(D_hadamard.get_mpz_t(), D_hadamard1.get_mpz_t());
 
+		cout << "\nprvocisla: ";
+		for(int i = 0; i <= poc_sieve; i ++)
+		{
+			if(sieve[i]) cout << i << ", ";
+		}
+		cout << "\nD = " << ((long)D_hadamard.get_d()) << "\n";
 
 		
 
+		// TODO: funkce gcd(M,D), zvolit m_1, m_2... m_r
+		//       nasobit prvocisla, ktera nedeli D_hadamard, do te doby nez budou vetsi nez M
+		int M_akt=1;
+		cout << endl << M << " = 1 ";
+		for(int i=0;i<poc_sieve;i++)
+		{
+			if(sieve[i])
+			{
+				mpz_class delitel;
+				mpz_class prvoc=i;
+				mpz_gcd(delitel.get_mpz_t(), prvoc.get_mpz_t(), D_hadamard.get_mpz_t());
+				int idelitel=(int)delitel.get_d();
+				
+				if(idelitel==1)
+				{
+					M_akt*=i;
+					cout << i << " * ";
+				}else
+				{
+					cout << "(" << i << ") * ";
+				}
+			
+				if( M_akt > M )
+				{
+					break;
+				}
+			}
+		}
+		
 
+		//delete[] sieve;
+		//*/
 	}
 	void vypsat()
 	{
@@ -266,10 +353,49 @@ public:
 		{
 			for(int x=0;x<N;x++)
 			{
-				printf("%7.2f", pointer[x*N+y]);
+				printf("%7.2f", get_cell(x, y));
 			}
 			printf(" | %7.2f\n", prava_strana[y]);
 		}
 	}
 };
 
+// frexp, ldexp
+template<class T>
+int ifrexp(T X, int * Y)
+{
+	float a=frexp(X, Y);
+	int exponent;
+	for(exponent=1;exponent<numeric_limits<T>::digits;exponent++)
+	{
+		a=2.0*a;
+		if( a == floor(a) ) break;
+	}
+	(*Y)-=exponent;
+	return (int)a;
+}
+
+unsigned int gcd(unsigned int u, unsigned int v)
+{
+  // simple cases (termination)
+  if (u == v)
+    return u;
+  if (u == 0)
+    return v;
+  if (v == 0)
+    return u;
+ 
+  // look for factors of 2
+  if (~u & 1) // u is even
+    if (v & 1) // v is odd
+      return gcd(u >> 1, v);
+    else // both u and v are even
+      return gcd(u >> 1, v >> 1) << 1;
+  if (~v & 1) // u is odd, v is even
+    return gcd(u, v >> 1);
+ 
+  // reduce larger argument
+  if (u > v)
+    return gcd((u - v) >> 1, v);
+  return gcd((v - u) >> 1, u);
+}
