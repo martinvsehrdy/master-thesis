ec2470435526a34ec4b752b22ceea7b69163deb7
cernym16 <cernym16@fit.cvut.cz>
2013-01-09 23:21:43 +0100

    vyřešení SLR v modulech a převod do celých čísel (tam bude asi chyba)

--- diplomka/diplomka.cpp
+++ diplomka/diplomka.cpp
@@ -4,6 +4,7 @@
 #include "stdafx.h"
 #include <string>
 #include <iostream>
+#include <sstream>
 #include <iomanip>
 #include "matice.h"
 #include <list>
@@ -11,46 +12,45 @@
 using namespace std;
 
 
-int main(int argc, char* argv[])
+int main(int argc, char** argv)
+// argv[0]
 {
-	/*mpz_class aBigPO2;
-
-    aBigPO2 = 1073741824; //2^30
-    aBigPO2*=aBigPO2; //2^60
-    aBigPO2*=aBigPO2; //2^120
-    aBigPO2*=aBigPO2; //2^240
-    aBigPO2*=aBigPO2; //2^480
-    aBigPO2*=aBigPO2; //2^960
-    aBigPO2*=aBigPO2; //2^1920
-
-    cout << aBigPO2 << endl;
-	//*/
-
-	bool yes=false;
-	unsigned short s=65530;
-	if(s+30>s)
-		yes=true;
-
-	float x=1.0/(float)(1 << 20);
-	
-	int e;
-	float a=frexp(x, &e);
-	int v=(int)(a*(1 << numeric_limits<float>::digits));
-	e-=numeric_limits<float>::digits;
-	yes=false;
-	if(x==ldexp((float)v, e))
-		yes=true;
-
-
-	
-	matice<float> mat(4);
-	mat.load_matrix("mat2.txt");
-	mat.vypsat();
-	mat.do_modular();
-	cout << "VYSLEDEK:" << endl;
-	mat.vypsat();
-	//*/
+	/*int modulo=3;
+	for(int a=-10;a<10;a++)
+	{
+		int mod=a % modulo;
+		if(mod<0) mod+=modulo;
+		cout << a << " -> " << mod << " -> " << m_inv(modulo, mod) << endl;	// TODO: moduluje pouze kladne
+	}
 	cin.get();
 	return 0;
-}
+	/*/
 
+	int size=0;
+	try
+	{
+		if(argc>1)
+		{
+			size=atoi(argv[1]);
+		}
+	}catch(...)
+	{
+		printf("nekde se stala chyba :-(\nspravne volani programu: %s ", argv[0]);
+		return 0;
+	}
+
+
+	matice<double> mat(size);
+	
+	mat.fill_hilbert();
+	mat.load_matrix("mat1.txt");
+	
+	mat.vypsat();
+	mat.do_modular();
+	//mat.do_gauss();
+	cout << "VYSLEDEK:" << endl;
+	mat.vypsat();
+	
+	cin.get();
+	return 0;//*/
+}
--- diplomka/matice.h
+++ diplomka/matice.h
@@ -5,6 +5,7 @@
 #include <time.h>
 #include <iostream>
 #include <fstream>
+#include <list>
 
 using namespace std;
 
@@ -28,9 +29,12 @@ public:
 	}
 	matice(int size)
 	{
-		this->N=size;
-		pointer=new T[size*size];
-		prava_strana=new T[size];
+		if(size>0)
+		{
+			this->N=size;
+			pointer=new T[size*size];
+			prava_strana=new T[size];
+		}
 	}
 	matice(int size, T value)
 	{
@@ -62,6 +66,10 @@ public:
 	int get_size()
 	{
 		return N;
+	}
+	int get_index(int X, int Y)
+	{
+		return X*N+Y;
 	}
 	int set_cell(int X, int Y, T value)
 	{
@@ -109,20 +117,32 @@ public:
 			prava_strana[i]=(rand() % 1000 + 1)/10.0;
 		}
 	}
-	void save_matrix(char* filename)
+	void fill_hilbert(void)
 	{
-		FILE* file;
-		file=fopen(filename, "w");
-		fprintf(file, "%d\n", N);
 		for(int y=0;y<N;y++)
 		{
 			for(int x=0;x<N;x++)
 			{
-				fprintf(file, "%7.2f", pointer[x*N+y]);
+				T val=1.0/((T)(1+x+y));
+				set_cell(x, y, val);
 			}
-			fprintf(file, " | %7.2f\n", prava_strana[y]);
+			prava_strana[y]=(y+1)*(y+1);
 		}
-		fclose(file);
+	}
+	void save_matrix(char* filename)
+	{
+		fstream file;
+		file.open(filename, fstream::out);
+		file << N << endl;
+		for(int y=0;y<N;y++)
+		{
+			for(int x=0;x<N;x++)
+			{
+				file << get_cell(x, y) << "\t";
+			}
+			file << prava_strana[y] << endl;
+		}
+		file.close();
 	}
 	int load_matrix(char* filename)
 	{
@@ -207,10 +227,6 @@ public:
 			set_cell(ipivot, ipivot, 1.0);
 		}
 	}
-	int hadamard()
-	{
-		
-	}
 	void do_modular(void)
 	{
 		if(pointer==NULL || prava_strana==NULL) return;
@@ -281,13 +297,14 @@ public:
 		}
 		
 		// hadamard
+		cout << "\nhadamard ";
 		mpz_class D_hadamard1=1;
 		for(int iX=0;iX<N;iX++)
 		{
 			mpz_class souc=0;
 			for(int iY=0;iY<N;iY++)
 			{
-				souc+=((long)get_cell(iX, iY))*((long)get_cell(iX, iY));
+				souc+=(get_cell(iX, iY))*(get_cell(iX, iY));
 			}
 			double dh=D_hadamard1.get_d();
 			double ds=souc.get_d();
@@ -295,44 +312,44 @@ public:
 			mpz_class delitel;
 			mpz_gcd(delitel.get_mpz_t(), souc.get_mpz_t(), D_hadamard1.get_mpz_t());
 			int idelitel=(int)delitel.get_d();
+			if(delitel>1) cout << delitel << " ";
 			//sieve[idelitel]=false;	// nevyhovujici prvocislo/slozene cislo
 			//D_hadamard1/=delitel;
 			//souc/=delitel;
 			D_hadamard1*=souc;
 		}
+		double dh=D_hadamard1.get_d();
+		mpz_class D_hadamard=sqrt(D_hadamard1);
 		
-		mpz_class D_hadamard;
-		mpz_sqrt(D_hadamard.get_mpz_t(), D_hadamard1.get_mpz_t());
 
 		cout << "\nprvocisla: ";
 		for(int i = 0; i <= poc_sieve; i ++)
 		{
-			if(sieve[i]) cout << i << ", ";
+			//if(sieve[i]) cout << i << ", ";
 		}
-		cout << "\nD = " << ((long)D_hadamard.get_d()) << "\n";
+		cout << "\nD = " << D_hadamard << "\n";
 
 		
 
-		// TODO: funkce gcd(M,D), zvolit m_1, m_2... m_r
-		//       nasobit prvocisla, ktera nedeli D_hadamard, do te doby nez budou vetsi nez M
-		int M_akt=1;
-		cout << endl << M << " = 1 ";
+		// zvolit m_1, m_2... m_r
+		// nasobit prvocisla, ktera nedeli D_hadamard, do te doby nez budou vetsi nez M
+		mpz_class M_akt=1;
+		list<int> m_prvocisla;
+		m_prvocisla.clear();
 		for(int i=0;i<poc_sieve;i++)
 		{
 			if(sieve[i])
 			{
+				// TODO: zjistit proc se 3 vyskytuje mezi m_prvocisla kdyz 3^2 deli D_hadamard1
 				mpz_class delitel;
 				mpz_class prvoc=i;
 				mpz_gcd(delitel.get_mpz_t(), prvoc.get_mpz_t(), D_hadamard.get_mpz_t());
 				int idelitel=(int)delitel.get_d();
 				
-				if(idelitel==1)
+				if(delitel==1)
 				{
 					M_akt*=i;
-					cout << i << " * ";
-				}else
-				{
-					cout << "(" << i << ") * ";
+					m_prvocisla.push_front(i);
 				}
 			
 				if( M_akt > M )
@@ -341,21 +358,196 @@ public:
 				}
 			}
 		}
+		cout << "M     = " << ((mpz_class)M) << " = ";
+		// TODO: vymazat zbytecna prvocisla napr. 2, 3..
+		/*do
+		{
+			M_akt*=(*m_iter);
+			cout << (*m_iter);
+			if(M_akt > M)
+			{
+				list<int>::iterator rem=m_iter;
+				m_iter++;
+				m_prvocisla.remove((*rem));
+				m_iter--;
+				//break;
+			}else cout << " * ";
+		}while(
+			*/
+		cout << endl << "M_akt = " << M_akt << endl;
+		// TODO: spustit reseni v jednotlivych modulech
+		int** m_prava_strana=new int*[m_prvocisla.size()];
+		int** m_diagonala=new int*[m_prvocisla.size()];
+		int* m_matice=new int[N*N];
+		int m_i=0;
+		for(m_i=0;m_i<m_prvocisla.size();m_i++)
+		{
+			m_prava_strana[m_i]=new int[N];
+			m_diagonala[m_i]=new int[N];
+			for(int i=0;i<N;i++)
+			{
+				m_prava_strana[m_i][i]=i;
+				m_diagonala[m_i][i]=i;
+			}
+		}
+		m_i=0;
+		for(list<int>::iterator m_iter=m_prvocisla.begin();m_iter!=m_prvocisla.end();m_iter++)
+		{
+			for(int i=0;i<N;i++)
+			{
+				m_prava_strana[m_i][i]=9;
+				m_diagonala[m_i][i]=8;
+			}
+			cout << endl;
+			// zkopirovat a modulovat
+			for(int y=0;y<N;y++)
+			{
+				int pom;
+				for(int x=0;x<N;x++)
+				{
+					pom=((long)get_cell(x,y)) % (*m_iter);
+					//if( pom==0 ) pom=(*m_iter);
+					m_matice[get_index(x,y)]=pom;
+				}
+				pom=((long)prava_strana[y]) % (*m_iter);
+				//if( pom==0 ) pom=(*m_iter);
+				m_prava_strana[m_i][y]=pom;
+			}
+
+
+			// spocitat gauss-jordanovu eliminaci
+			// TODO: posouvat cisla v radcich doleva, kvuli CUDA, aby se pristupovalo stale na ty stejna mista v pameti, 
+			//       vysledek bude v prvnim sloupci matice
+			for(int ipivot=0;ipivot<N;ipivot++)
+			{
+				// deleni nulou => nasobeni inverznim prvkem
+				if(m_matice[get_index(ipivot, ipivot)]==0)
+				{
+					// v 'ipivot'-tem radku na diagonále je nula => vymena s jinym radkem
+					int novy_pivot=ipivot;
+					do{
+						novy_pivot++;
+					}while(m_matice[get_index(ipivot, novy_pivot)]==0 && novy_pivot<N);
+
+					if(m_matice[get_index(ipivot, novy_pivot)]!=0 && novy_pivot<N)		// nasel jsem radek s nenulovym prvkem ve sloupci ipivot
+					{
+						// vymena radku ipivot a novy_pivot
+						double pom;
+						for(int iX=0;iX<N;iX++)
+						{
+							pom=m_matice[get_index(iX, ipivot)];
+							m_matice[get_index(iX, ipivot)]=m_matice[get_index(iX, novy_pivot)];
+							m_matice[get_index(iX, novy_pivot)]=pom;
+						}
+						pom=m_prava_strana[m_i][ipivot];
+						m_prava_strana[m_i][ipivot]=m_prava_strana[m_i][novy_pivot];
+						m_prava_strana[m_i][novy_pivot]=pom;
+					}else
+					{
+						// matice nema v 'ipivot'-tem sloupci nenulovy prvek => je singularni
+						//cout << "singularni" << endl;
+						for(int i=0;i<N;i++)	// singularni matice => vysledky jsou nulove (nepouzitelne)
+						{
+							m_prava_strana[m_i][i]=0;
+							m_diagonala[m_i][i]=0;
+						}
+						//break;
+					}
+				}
+				int inverzni=m_inv((*m_iter), m_matice[get_index(ipivot, ipivot)]);
+
+				for(int iY=0;iY<N;iY++)	// prochazi jednotlive radky
+				{
+					if(iY==ipivot) continue;
+					int pom;
+					int multipl=(m_matice[get_index(ipivot, iY)]*inverzni) % (*m_iter);
+					for(int iX=0;iX<N;iX++)	// prochazi cisla v i1-tem radku
+					{
+						int m1=m_matice[get_index(iX, iY)];
+						int m2=m_matice[get_index(iX, ipivot)];
+						pom = m1-multipl*m2;
+						pom=pom % (*m_iter);
+						if(pom<0) pom+=(*m_iter);
+						m_matice[get_index(iX, iY)]=pom;
+					}
+					pom = m_prava_strana[m_i][iY]-multipl*m_prava_strana[m_i][ipivot];
+					m_prava_strana[m_i][iY]=pom % (*m_iter);
+					if(m_prava_strana[m_i][iY]<0) m_prava_strana[m_i][iY]+=(*m_iter);
+				}
+				// ulozit diagonalu do m_diagonala
+				for(int iX=0;iX<N;iX++)
+				{
+					m_diagonala[m_i][iX]=m_matice[get_index(iX, iX)];
+				}
+				
+			}
+			//*/
+			
+			m_i++;
+		}
 		
+		delete[] m_matice;
+		// vypsat vysledky m_prava_strana
+		cout << "VYSLEDKY: " << endl;
+		for(list<int>::iterator m_iter=m_prvocisla.begin();m_iter!=m_prvocisla.end();m_iter++)
+		{
+			cout << (*m_iter) << "\t";
+		}
+		cout << endl;
+		for(int y=0;y<N;y++)
+		{
+			m_i=0;
+			for(list<int>::iterator m_iter=m_prvocisla.begin();m_iter!=m_prvocisla.end();m_iter++)
+			{
+				cout << m_prava_strana[m_i][y] << "/" << m_diagonala[m_i][y] << "\t";
+				m_i++;
+			}
+			cout << endl;
+		}
+		// TODO: z vysledku m_prava_strana ziskat celociselne vysledky - mixed-radix
+		for(int y=0;y<N;y++)
+		{
+			mpz_class citatel=0;
+			mpz_class jmenovatel=0;
+			mpz_class radix=1;
+			m_i=0;
+			for(list<int>::iterator m_iter=m_prvocisla.begin();m_iter!=m_prvocisla.end();m_iter++)
+			{
+				cout << "+" << m_prava_strana[m_i][y] <<"*" << radix;
+				//m_prava_strana[m_i][y]
+				citatel+=m_prava_strana[m_i][y]*radix;
+				jmenovatel+=m_diagonala[m_i][y]*radix;
+				radix*=(*m_iter);
+
+				m_i++;
+			}
+			cout << endl;
+			prava_strana[y]=citatel.get_d()/jmenovatel.get_d();
+			cout << citatel << "/" << endl << jmenovatel << " = " << prava_strana[y] << endl;
+		}
 
 		//delete[] sieve;
+		for(int i=0;i<m_prvocisla.size();i++)
+		{
+			//delete[] m_prava_strana[m_i];
+			//delete[] m_diagonala[m_i];
+		}
+		//delete[] m_prava_strana;
+		//delete[] m_diagonala;
 		//*/
 	}
 	void vypsat()
 	{
 		printf("\n");
-		for(int y=0;y<N;y++)
+		for(int y=0;y<min(N,6);y++)
 		{
-			for(int x=0;x<N;x++)
+			int x;
+			for(x=0;x<min(N,5);x++)
 			{
-				printf("%7.2f", get_cell(x, y));
+				printf("%7.2f ", get_cell(x, y));
 			}
-			printf(" | %7.2f\n", prava_strana[y]);
+			if(x<N-1) printf("...");
+			printf("| %7.2f\n", prava_strana[y]);
 		}
 	}
 };
@@ -375,27 +567,14 @@ int ifrexp(T X, int * Y)
 	return (int)a;
 }
 
-unsigned int gcd(unsigned int u, unsigned int v)
+int m_inv(int modulo, int cislo)
 {
-  // simple cases (termination)
-  if (u == v)
-    return u;
-  if (u == 0)
-    return v;
-  if (v == 0)
-    return u;
- 
-  // look for factors of 2
-  if (~u & 1) // u is even
-    if (v & 1) // v is odd
-      return gcd(u >> 1, v);
-    else // both u and v are even
-      return gcd(u >> 1, v >> 1) << 1;
-  if (~v & 1) // u is odd, v is even
-    return gcd(u, v >> 1);
- 
-  // reduce larger argument
-  if (u > v)
-    return gcd((u - v) >> 1, v);
-  return gcd((v - u) >> 1, u);
+	// TODO
+	if(cislo==0) return 0;
+	int i;
+	for(i=1;i<=modulo;i++)
+	{
+		if( (cislo*i)% modulo==1 ) break;
+	}
+	return i;
 }
